8f0b9deb350a3c311bab123d39fb7392925c123b
José Messias <104660308+JoseCMessias@users.noreply.github.com>
2022-12-12 17:06:00 -0300

    Add files via upload

---
+++ trilha-net-poo-desafio/Program.cs
@@ -0,0 +1,26 @@
+﻿using DesafioPOO.Models;
+using System;
+
+namespace DesaioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Smartphone Iphone");
+            Smartphone iphone = new Iphone("81984550432", "iphone 11", "121212", 120);
+            iphone.Ligar();
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Instagram");
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Smartphone Nokia");
+            Smartphone nokia = new Nokia("81985345525", "nokia C20", "131313", 64);
+            nokia.Ligar();
+            nokia.ReceberLigacao();
+            nokia.InstalarAplicativo("Facebook");
+        }
+    }
+}
+
