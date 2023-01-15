33016965139d045d3aa717aca4cb0630fac98fc6
JoseCMessias <josecmessias22@gmail.com>
2023-01-15 12:54:28 -0300

    Update

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using DesafioPOO.Models;
+using System;
+
+namespace DesaioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Smartphone nokia = new Nokia("81985345525", "nokia C20", "131313", 64);
+            Smartphone iphone = new Iphone("81984550432", "iphone 11", "121212", 120);
+            
+            Console.WriteLine("========== Smartphone Iphone ==========");
+            iphone.InstalarAplicativo("WhatsApp");
+            nokia.Ligar();
+
+            Console.WriteLine("=======================================");
+
+            Console.WriteLine("========== Smartphone Nokia ==========");
+            nokia.InstalarAplicativo("WhatsApp");
+            iphone.ReceberLigacao();    
+        }
+    }
+}
+
